954e76886c8cf406f5c7d2aa32370ca4e396f937
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-09-21 16:29:53 +0200

    install Eassyadmin with Composer

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
